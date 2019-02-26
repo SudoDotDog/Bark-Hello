eb5a4fac22eda8a2bb3155de186a5cbab356ed3c
WMXPY <wm@mengw.io>
2019-02-26 11:26:09 -0600

    remove redux

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,12 @@
  * @description Index
  */
 
-import { SudooProvider } from "@sudoo/redux";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
 import { HashRouter } from "react-router-dom";
 import '../style/common/global.sass';
 import Entry from "./entry";
-import { redux } from "./state/store";
 
 declare const module: any;
 
@@ -19,11 +17,9 @@ const render: (App: any) => void = (App: any): void => {
 
     ReactDOM.render(
         (<AppContainer>
-            <SudooProvider redux={redux}>
-                <HashRouter>
-                    <App />
-                </HashRouter>
-            </SudooProvider>
+            <HashRouter>
+                <App />
+            </HashRouter>
         </AppContainer>),
         document.getElementById("container"));
 };
