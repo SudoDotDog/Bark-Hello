8e3517f018a6fc5d6c1dc41701f3c25b079089d9
WMXPY <wm@mengw.io>
2019-03-01 14:29:57 -0600

    use global form dialog

--- src/entry.tsx
+++ src/entry.tsx
@@ -6,17 +6,26 @@
 
 import * as React from "react";
 import { Redirect, Route } from "react-router-dom";
+import * as entryStyle from "../style/entry.scss";
+import { GatherAddName } from "./form/add-name";
 import { Grid } from "./page/grid";
+import { Nav } from "./page/nav";
 
 export class Entry extends React.Component {
 
     public render(): JSX.Element {
 
         return (
-            <React.Fragment>
+            <div className={entryStyle.entry}>
                 <Route path="/" exact render={() => <Redirect to="/grid" />} />
-                <Route path="/grid" component={Grid} />
-            </React.Fragment>
+                <GatherAddName />
+                <div className={entryStyle.content}>
+                    <Route path="/grid" component={Grid} />
+                </div>
+                <div className={entryStyle.nav}>
+                    <Nav />
+                </div>
+            </div>
         );
     }
 }
---
+++ style/entry.scss.d.ts
@@ -0,0 +1,3 @@
+export const entry: string;
+export const content: string;
+export const nav: string;
